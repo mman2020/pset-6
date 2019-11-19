11a380d3db59368baadbaafaaf9eebd263c2b659
Matthew Man <mman@ucvts.org>
2019-11-19 09:16:27 -0500

    removed a few constructors to allow allow code to compile

--- src/ATM.java
+++ src/ATM.java
@@ -7,17 +7,18 @@ public class ATM {
 	private BankAccount activeAccount;
 	private Bank bank;
 	    
-	 public static final int VIEW = 1;
-	 public static final int DEPOSIT = 2;
-	 public static final int WITHDRAW = 3;
-	 public static final int TRANSFER = 4;
-	 public static final int LOGOUT = 5;
-	 public static final int FIRST_NAME_WIDTH = 20;
-	 public static final int LAST_NAME_WIDTH = 20;	
-	 
-	 public static final int INVALID = 0;
-     public static final int INSUFFICIENT = 1;
-     public static final int SUCCESS = 2; 
+	public static final int VIEW = 1;
+	public static final int DEPOSIT = 2;
+	public static final int WITHDRAW = 3;
+	public static final int TRANSFER = 4;
+	public static final int LOGOUT = 5;	
+ 
+	public static final int INVALID = 0;
+	public static final int INSUFFICIENT = 1;
+	public static final int SUCCESS = 2; 
+ 
+	public static final int FIRST_NAME_WIDTH = 20;
+	public static final int LAST_NAME_WIDTH = 20;
    
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
