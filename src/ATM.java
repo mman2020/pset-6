2d52a3493a7636b129dd67ea955627dc7ee9c632
Matthew Man <mman@ucvts.org>
2019-11-19 09:20:29 -0500

    added int for char limit

--- src/ATM.java
+++ src/ATM.java
@@ -17,8 +17,8 @@ public class ATM {
 	public static final int INSUFFICIENT = 1;
 	public static final int SUCCESS = 2; 
  
-	public static final int FIRST_NAME_WIDTH = 20;
-	public static final int LAST_NAME_WIDTH = 20;
+	public static final int FIRST_NAME_WIDTH = 40;
+	public static final int LAST_NAME_WIDTH = 40;
    
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
