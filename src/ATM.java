8e67134fa152610888189a761e9d73e0d9d110d7
Matthew Man <mman@ucvts.org>
2019-11-22 11:33:07 -0500

    modified createaccount function to fix exception error

--- src/ATM.java
+++ src/ATM.java
@@ -121,7 +121,6 @@ public class ATM {
         	int pin = in.nextInt();
         	in.nextLine();
         	newUser = new User(firstName, lastName);
-        	
         	bank.createAccount(pin, newUser);
         	bank.save();
         	return;
